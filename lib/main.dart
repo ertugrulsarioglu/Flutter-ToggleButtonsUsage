5f2d4dac79ca8884a249f76b86e27b51f2c35d5f
ertugrul <sariertugrull@gmail.com>
2022-05-16 03:18:13 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var toggleDurumlar = [false,true,false];
+  int secilenToggleIndeks = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ToggleButtons(
+                children: [
+                  Icon(Icons.looks_one),
+                  Icon(Icons.looks_two),
+                  Icon(Icons.looks_3),
+                ],
+                isSelected: toggleDurumlar,
+              color: Colors.pink,
+              selectedColor: Colors.deepPurple,
+              fillColor: Colors.yellow,
+              onPressed: (int secilenIndeks){
+                  secilenToggleIndeks = secilenIndeks;
+                  print("${secilenIndeks+1}. toggle seçildi");
+
+                  setState(() {
+                    toggleDurumlar[secilenIndeks] = !toggleDurumlar[secilenIndeks];
+                  });
+
+              },
+            ),
+            ElevatedButton(
+              child: Text("Göster"),
+              onPressed: (){
+                print("En son ${secilenToggleIndeks+1}. toggle seçildi");
+              },
+            ),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
